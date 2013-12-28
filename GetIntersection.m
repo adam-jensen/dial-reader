96627a761c2a02e2c263fdcca60c192336211940
Adam Jensen <oojensen@stanford.edu>
2013-12-27 17:02:58 -0800

    least squares method

---
+++ GetIntersection.m
@@ -0,0 +1,11 @@
+%find the intersection of two lines in polar coordinates
+%result is either wrong, or not in image coordinates
+function point = GetIntersection(pair1, pair2)
+    line1 = polyfit(pair1(:, 1), pair1(:, 2), 1);
+    line2 = polyfit(pair2(:, 1), pair2(:, 2), 1);
+    %function searches near here
+    guess = 100;
+    x = fzero(@(x) polyval(line1 - line2, x), guess);
+    y = polyval(line1, x);
+    point = [x, y];
+end
