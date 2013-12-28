a460cf654cf7ac398c197c1f27b038f4a972bc8e
Adam Jensen <oojensen@stanford.edu>
2013-12-28 08:38:51 -0800

    fails to find the intersection sometimes

--- GetIntersection.m
+++ GetIntersection.m
@@ -4,8 +4,8 @@ function point = GetIntersection(pair1, pair2)
     line1 = polyfit(pair1(:, 1), pair1(:, 2), 1);
     line2 = polyfit(pair2(:, 1), pair2(:, 2), 1);
     %function searches near here
-    guess = 100;
-    x = fzero(@(x) polyval(line1 - line2, x), guess);
-    y = polyval(line1, x);
+    guess = 0;
+    x = fzero(@(x) (polyval(line1, x) - polyval(line2, x)), guess);
+    y = polyval(line2, x);
     point = [x, y];
 end
