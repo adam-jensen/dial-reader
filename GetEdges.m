eb56e94153a8abde46785006841d58e56d9cc1f4
Adam Jensen <oojensen@stanford.edu>
2013-12-28 08:36:57 -0800

    filter is now square

--- GetEdges.m
+++ GetEdges.m
@@ -1,8 +1,8 @@
 %extract the edges from the intensity values (could be a colormap one day)
 function edges = GetEdges(imdata)
     imdata = double(rgb2gray(imdata));
-    blackwhite = edge(imdata, 'sobel');
+    edges = edge(imdata, 'sobel');
     %dilate the image with a small structuring element
-    dilator = strel('disk', 1, 0);
-    edges = imdilate(blackwhite, dilator);
+    dilator = strel('square', 8);
+    edges = imdilate(edges, dilator);
 end
