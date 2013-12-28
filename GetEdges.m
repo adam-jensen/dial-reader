0473a7189374d84db14b5f59e8b4c93997b72621
Adam Jensen <oojensen@stanford.edu>
2013-12-28 09:55:35 -0800

    all colinear lines are the same now

--- GetEdges.m
+++ GetEdges.m
@@ -3,6 +3,6 @@ function edges = GetEdges(imdata)
     imdata = double(rgb2gray(imdata));
     edges = edge(imdata, 'sobel');
     %dilate the image with a small structuring element
-    dilator = strel('square', 8);
+    dilator = strel('square', 3);
     edges = imdilate(edges, dilator);
 end
